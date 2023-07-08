17ab4d46b55be70e44779c432271708c81014b23
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2023-07-08 10:48:53 +0200

    fix test

--- tests/integration/health.test.js
+++ tests/integration/health.test.js
@@ -1,10 +1,11 @@
 import request from 'supertest';
 import app from '../../app.js';
+import { apiOptions } from '../../src/configs/api.config.js';
 
 describe('Get healthcheck', () => {
   it('should call the health check', async () => {
     const res = await request(app)
-      .get('/api/health');
+      .get(`${apiOptions.prefix}/health`);
     expect(res.statusCode)
       .toEqual(200);
   });
